929312f2c177f875005f9fbbf5458fecbcbee89a
aizpurua23a <aizpurua23a@gmail.com>
2020-04-21 19:08:17 +0200

    Updates

---
+++ precache-manifest.6a61a0a36fdca1b80420722bafc3ccf3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96fcb044765b41caa60e225ebe04e11c",
+    "url": "/junkaltxorrarenbila/index.html"
+  },
+  {
+    "revision": "96a295bc40cd97063b38",
+    "url": "/junkaltxorrarenbila/static/css/main.2c5478c9.chunk.css"
+  },
+  {
+    "revision": "48d3e15aed299ec57578",
+    "url": "/junkaltxorrarenbila/static/js/2.e808b9a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/junkaltxorrarenbila/static/js/2.e808b9a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96a295bc40cd97063b38",
+    "url": "/junkaltxorrarenbila/static/js/main.ac241d57.chunk.js"
+  },
+  {
+    "revision": "16c6019636495007585c",
+    "url": "/junkaltxorrarenbila/static/js/runtime-main.d9fcbd01.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/junkaltxorrarenbila/static/media/logo.5d5d9eef.svg"
+  }
+]);
